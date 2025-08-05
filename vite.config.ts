508541968b51eca54d07f692d7b2222b57a2c077
sadiqkhan7777 <saiqkhan7777@gmail.com>
2025-08-05 20:29:54 +0500

    my react portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Vercel requires root base
   plugins: [react(),
     tailwindcss(),
   ],
