1f3c80bf698af4c506ab4584e29da26096388b3e
sadiqkhan7777 <saiqkhan7777@gmail.com>
2025-08-05 20:27:46 +0500

    my react portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/React-Simple-Portfolio/',
   plugins: [react(),
     tailwindcss(),
   ],
